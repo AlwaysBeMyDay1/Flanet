3921e87fc65a8b3d77d0e882223ca1d790398fb3
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-07-22 21:52:52 +0900

    🌱 Set User admin & users.urls

--- server/conf/urls.py
+++ server/conf/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/'. include('users.urls')),
+    path('api/v0/users/', include('users.urls')),
+    # path('api/v0/friends/', include('friends.urls')),
 ]
