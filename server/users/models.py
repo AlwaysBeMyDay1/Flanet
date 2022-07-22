a4ff622e329fbaf5ea80a69b34eddf9c2b500974
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-07-23 02:37:45 +0900

    🌿Login User

--- server/users/models.py
+++ server/users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    nickname = models.CharField(blank=False, unique=True ,max_length=30)
+    nickname = models.CharField(blank=True, max_length=30)
     social_url = models.CharField(blank=True, max_length=255)
     hometown = models.CharField(blank=True, max_length=255)
     memo = models.TextField(blank=False)
