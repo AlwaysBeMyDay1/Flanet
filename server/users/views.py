3c404f8331dac1812c95ba10528cc4144a0a83c7
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-07-22 21:55:47 +0900

    🌿 Add UserViewSet

--- server/users/views.py
+++ server/users/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.viewsets import ModelViewSet
 
+from .models import User
+from .serializers import UserSerializer
+
 class UsersViewSet(ModelViewSet):
-    pass
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsAdminUser]
+        elif self.action == 'create' or self.action == 'retrieve':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+
+# class GroupViewSet(ModelViewSet):
+#     serializer_class = GroupSerializer
+#     queryset = Group.objects.all()
+
+#     def get_permissions(self):
+#         if self.action == 'detail':
+#             permission_classes = [IsMember]
+#         return [permission() for permission in permission_classes]
