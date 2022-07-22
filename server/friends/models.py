c904b4672a4688c96ef30b358acf6cdc2b14bca6
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-07-23 02:36:52 +0900

    🌿Modified Friends Model

--- server/friends/models.py
+++ server/friends/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from users.models import Users
+from users.models import User
 
 
 class Friends(models.Model):
-    user_one = models.ForeignKey(Users, related_name="following", on_delete=models.CASCADE, blank=False) 
-    user_two = models.ForeignKey(Users, related_name="following", on_delete=models.CASCADE, blank=False)
-    created_date = models.DateTimeField(auto_now_add=True, blank=False) # 최초 저장 시에만 현재날짜 적용
+    user_one = models.ForeignKey(User, related_name="user_one", on_delete=models.CASCADE, blank=False) 
+    user_two = models.ForeignKey(User, related_name="user_two", on_delete=models.CASCADE, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True, blank=False)
     meeting_count = models.IntegerField(default=1)
     
     class Meta:
@@ -13,7 +13,3 @@ class Friends(models.Model):
             models.UniqueConstraint(fields=['user_one','user_two'],  name="unique_friends")
         ]
         ordering = ["-meeting_count"]
-
-    def __str__(self):
-        f"{self.user_one} became friend with {self.user_two}"
-
