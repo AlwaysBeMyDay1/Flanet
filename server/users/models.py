a0dd8cae00062419d9a194a0ae23d05df5e7f230
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-07-22 21:53:58 +0900

    🌱 Modified User Model

--- server/users/models.py
+++ server/users/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Users(AbstractUser):
+class User(AbstractUser):
     nickname = models.CharField(blank=False, unique=True ,max_length=30)
     social_url = models.CharField(blank=True, max_length=255)
     hometown = models.CharField(blank=True, max_length=255)
     memo = models.TextField(blank=False)
-    profile_image = models.CharField(blank=False)
+    profile_image = models.CharField(blank=False, max_length=30)
