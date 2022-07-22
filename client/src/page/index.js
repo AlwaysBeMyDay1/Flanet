2c69683d02529cf663546b0a562b24929c618c61
rkskekzzz <wkdlfflxh@naver.com>
2022-07-22 20:06:36 +0900

    🎨 프로젝트 구조 설계 및 react-router-dom설치
    Co-authored-by: jinyong <it794613@users.noreply.github.com>

---
+++ client/src/page/index.js
@@ -0,0 +1,8 @@
+import Planet from './Planet';
+import Profile from './Profile';
+import Register from './Register';
+import Signin from './Signin';
+import Splash from './Splash';
+import Error from './Error';
+
+export { Planet, Profile, Register, Signin, Splash, Error };
