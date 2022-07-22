14fc9454df8922ef4a90df9da98616d5dc41cef2
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-07-23 02:39:30 +0900

    🌿Modified Auth

--- server/conf/urls.py
+++ server/conf/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v0/users/', include('users.urls')),
-    # path('api/v0/friends/', include('friends.urls')),
+    path('api/v0/friends/', include('friends.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
